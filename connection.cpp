8201f3f73855425b5a73681d330a17c3d14a1177
Yasminee014 <tabbaneyasmine@gmail.com>
2025-10-17 11:27:28 +0100

    Add

--- connection.cpp
+++ connection.cpp
@@ -8,9 +8,9 @@ Connection::Connection()
 bool Connection::createconnect()
 {bool test=false;
     QSqlDatabase db = QSqlDatabase::addDatabase("QODBC");
-    db.setDatabaseName("projet-2A");//inserer le nom de la source de données
-    db.setUserName("yasmine");//inserer nom de l'utilisateur
-    db.setPassword("123");//inserer mot de passe de cet utilisateur
+    db.setDatabaseName("Source_projet_2A");//inserer le nom de la source de données
+    db.setUserName("houssem");//inserer nom de l'utilisateur
+    db.setPassword("esprit25");//inserer mot de passe de cet utilisateur
 
     if (db.open())
         test=true;
